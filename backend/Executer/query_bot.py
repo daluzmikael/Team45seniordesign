91167016980c5a542c8993dbc333adab0f161e1d
Shah Arian <arianshah1276@gmail.com>
2026-02-12 22:27:04 -0500

    Update query_bot.py

--- backend/Executer/query_bot.py
+++ backend/Executer/query_bot.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 # Setting up OpenAI client
-print("API Key Loaded:", os.getenv("OPENAI_API_KEY"))
+#print("API Key Loaded:", os.getenv("OPENAI_API_KEY"))
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 # 2. Connect to PostgreSQL (AWS RDS)
